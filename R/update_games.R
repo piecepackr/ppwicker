b543433bb0265cffc4a880b3848063169b0e61ed
Trevor L Davis <trevor.l.davis@gmail.com>
2022-02-15 20:58:29 -0800

    feat: 'games' argument for 'update_games()'
    * New `games` argument for `update_games()` which allows one to update
      the articles for the games in `games`
      using the wiki markup in the `txt` subdirectory of
      `tools::R_user_dir("ppwicker", "cache")`.
      Name needs to be the same as the wiki article URL.
    * Could manually update game metadata in the cache and then upload updates
      to the wiki instead of trying to mess with the regular expression tool...

--- R/update_games.R
+++ R/update_games.R
@@ -1,26 +1,48 @@
-#' Update Games wiki page
+#' Update Games wiki page(s)
 #'
-#' `update_games()` updates piecepack wiki Games page.
+#' `update_games()` updates piecepack wiki Games page(s) using `wikiput.pl`.
+#'
+#' You may need a wiki admin password to use this function,
+#' any attempt at using this function with a `NULL` password failed.
 #'
 #' @inheritParams build_games
 #' @param minor Is this a \sQuote{minor} wiki edit?
 #' @param summary Summary for wiki edit
 #' @param username Piecepack wiki username
-#' @param password Piecepack wiki password (optional)
+#' @param password Piecepack wiki password
+#' @param games If `NULL` (default) we update the "Games" wiki page.
+#'              Otherwise we update the wiki page for each game in `games`
+#'              using the wiki markup in the `txt` subdirectory of
+#'              `tools::R_user_dir("ppwicker", "cache")`.
+#'              Name needs to be the same as the wiki article URL.
 #'
 #' @export
 update_games <- function(update = FALSE,
                          minor = FALSE,
                          summary = "Update Games page",
                          username = getOption("ppwicker_username"),
-                         password = getOption("ppwicker_password")) {
+                         password = getOption("ppwicker_password"),
+                         games = NULL) {
 
     stopifnot(!is.null(username))
 
-    text <- build_games(update = update)
-    wikiput(text, "https://ludism.org/ppwiki/GamesAlpha",
-            minor = minor, summary = summary,
-            username = username, password = password)
+    if (is.null(games)) {
+        target <- paste0("https://ludism.org/ppwiki/", "GamesAlpha")
+        text <- build_games(update = update)
+        wikiput(text, target,
+                minor = minor, summary = summary,
+                username = username, password = password)
+    } else {
+        for (game in games) {
+            target <- paste0("https://ludism.org/ppwiki/", game)
+            cache_dir <- tools::R_user_dir("ppwicker", "cache")
+            file <- file.path(cache_dir, "txt", game)
+            text <- readLines(file)
+            wikiput(text, target,
+                    minor = minor, summary = summary,
+                    username = username, password = password)
+        }
+    }
 }
 
 wikiput <- function(text, target, minor = FALSE, summary = NULL, username = NULL, password = NULL) {
