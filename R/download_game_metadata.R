1cd61bfa5eebf4fa8fa1513dd2e57f6f251d8242
Trevor L Davis <trevor.l.davis@gmail.com>
2022-02-15 20:41:43 -0800

    feat: 'games' argument for 'download_game_metadata()'
    * New `games` for `download_game_metadata()` allows only
      downloading game metadata for a subset of games.

--- R/download_game_metadata.R
+++ R/download_game_metadata.R
@@ -3,11 +3,13 @@
 #' `download_game_metadata()` downloads piecepack wiki game metadata.
 #'
 #' @param update If already downloaded previously, should we download again?
+#' @param games Which games to download metadata for.
+#'              If `NULL` (the default) download all of them.
 #' @return Returns a (tibble) data frame of piecepack wiki game metadata.
 #'         If no previous download or `update = TRUE` as a side effect
 #'         we download piecepack wiki game metadata to `tools::R_user_dir("ppwicker", "cache")`.
 #' @export
-download_game_metadata <- function(update = FALSE) {
+download_game_metadata <- function(update = FALSE, games = NULL) {
     cache_dir <- tools::R_user_dir("ppwicker", "cache")
     if (update) unlink(cache_dir)
     dir.create(cache_dir, showWarnings = FALSE)
@@ -20,14 +22,17 @@ download_game_metadata <- function(update = FALSE) {
         download.file("https://ludism.org/ppwiki/CategoryGame", games_file)
     }
     doc <- xml2::read_html(games_file)
-    games <- xml2::xml_find_all(doc, "//li/a")
-    links <- xml2::xml_attr(games, "href")
+    games_list <- xml2::xml_find_all(doc, "//li/a")
+    links <- xml2::xml_attr(games_list, "href")
     links <- links[-grep("GameTemplate", links)]
     raw_links <- gsub("ppwiki", "ppwiki/raw", links)
-    targets <- paste0(cache_dir, "/txt/", basename(raw_links), ".txt")
+    targets <- paste0(cache_dir, "/txt/", basename(raw_links))
 
     if (update) {
         for (i in seq_along(raw_links)) {
+            if (!is.null(games) &&
+                !(utils::URLdecode(basename(raw_links[i])) %in% utils::URLdecode(games)))
+                next
             Sys.sleep(1) # don't hit ludism.org too hard all at once
             download.file(raw_links[i], targets[i])
         }
